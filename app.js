38eaac84de1e8cdd6612b3b24fa043ef63ca2832
Lee Pierson <awesomehenge@gmail.com>
2023-06-08 16:06:01 +0100

    forgot to change this before push

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//let userName = prompt("Hi User :) Please tell me your name!")
-//console.log(userName);
+let userName = prompt("Hi User :) Please tell me your name!")
+console.log(userName);
 
-//if (confirm("Did you say " + userName)){
+if (confirm("Did you say " + userName)){
     const welcomeMsg = alert("Welcome to my page " + userName);
-//}else {const welcomeMsg = alert("Hello Stranger");
-//}
+}else {const welcomeMsg = alert("Hello Stranger");
+}
 
 
 function changeColor(colorName) {
