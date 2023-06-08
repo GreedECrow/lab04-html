289ff1c3c3eb40df565129b9e1592f54777451b1
Lee Pierson <awesomehenge@gmail.com>
2023-06-08 15:59:44 +0100

    first go at javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+//let userName = prompt("Hi User :) Please tell me your name!")
+//console.log(userName);
+
+//if (confirm("Did you say " + userName)){
+    const welcomeMsg = alert("Welcome to my page " + userName);
+//}else {const welcomeMsg = alert("Hello Stranger");
+//}
+
+
+function changeColor(colorName) {
+    document.body.style.background = colorName;
+}
